31f49f131df3d51f87acc413a1914b3ce24ecfdd
bahaa1985 <bahaa_salaheldin@live.com>
2022-09-26 01:30:54 +0200

    final result!

--- tables.js
+++ tables.js
@@ -1,34 +1,40 @@
 gsap.registerPlugin(ScrollTrigger)
-let window_width=window.innerWidth
+let window_width=0
 let tables_containers=gsap.utils.toArray('.table-content')
 
+//get the current width while resizing for gsap scroll purposes
+window.addEventListener('resize',()=>{
+    window_width=window.innerWidth
+})
 
-if(window_width>800){
-    gsap.to('.table-content',{scrollTrigger:{
-        trigger:'.section-tables',
-        start:'top center'
-    },opacity:1,duration:1,stagger:0.25}
-    )
-}
-else{
-   
-    tables_containers.forEach(table => {       
-        gsap.to(table,{scrollTrigger:{
-            trigger:table,
-            start:'top 50%',
-            snap:1
-        },opacity:1,duration:1}
-        ) 
-    });
+//activate scroll trigger effect while scrolling:
+document.addEventListener('scroll',()=>{
+    if(window_width>800){
+        gsap.to('.table-content',{scrollTrigger:{
+            trigger:'.section-tables',
+            start:'top center'
+        },opacity:1,duration:1,stagger:0.25}
+        )
+    }
+    else{
+       
+        tables_containers.forEach(table => {       
+            gsap.to(table,{scrollTrigger:{
+                trigger:table,
+                start:'top 50%'
+            },opacity:1,duration:1}
+            ) 
+        });
+ 
+    }
+})
 
-    
-}
 
-let header=document.getElementsByTagName('header')[0]
-let header_bars=document.getElementsByClassName('header-bars')[0]
-let header_links=document.getElementsByClassName('header-links')[0]
-let home_section=document.getElementsByClassName('home-section')[0]
-let home_top=0
+// let header=document.getElementsByTagName('header')[0]
+// let header_bars=document.getElementsByClassName('header-bars')[0]
+// let header_links=document.getElementsByClassName('header-links')[0]
+// let home_section=document.getElementsByClassName('home-section')[0]
+// let home_top=0
 
 
 // window.addEventListener('resize',()=>{
@@ -41,30 +47,30 @@ let home_top=0
 //     }
 // })
 
-document.addEventListener('scroll',()=>{
-    if(window_width>=800) {
-        home_top=home_section.getBoundingClientRect().y
-        if(home_top<0){
-            header.style.backgroundColor='black'
-        }
-        else{
-            header.style.backgroundColor='transparent'
-        }
-    }   
-})
+// document.addEventListener('scroll',()=>{
+//     if(window_width>=800) {
+//         home_top=home_section.getBoundingClientRect().y
+//         if(home_top<0){
+//             header.style.backgroundColor='black'
+//         }
+//         else{
+//             header.style.backgroundColor='transparent'
+//         }
+//     }   
+// })
 
-let toggled=false
-function clickBars(){
-    if(!toggled) {
-        toggled=true
-        header_links.style.display='block'  
-        header_links.className=''      
-    }
-    else {
-        toggled=false
-        header_links.style.display='none'
-    }
-}
+// let toggled=false
+// function clickBars(){
+//     if(!toggled) {
+//         toggled=true
+//         header_links.style.display='block'  
+//         header_links.className=''      
+//     }
+//     else {
+//         toggled=false
+//         header_links.style.display='none'
+//     }
+// }
 
 
 
